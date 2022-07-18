8d59be2fcf41feda5985fb7448ba22eab507b791
EmilEinarsen <emil@hagatun.se>
2022-07-18 09:25:33 -0400

    docs: document game constants

--- GameEngine/utils/constants.js
+++ GameEngine/utils/constants.js
@@ -10,9 +10,30 @@ export const GAME_OVER_SUBTITLE_FONT = getFontString()
 
 export const GRID_LINE_COLOR = '#232332'
 
+/**
+ * Controls the application framerate. 
+ * Should be coordinated with `SNAKE_MOVE_DELAY`
+ */
 export const FPS = 120
+/**
+ * Impacts the speed at which the Snake moves in relationship to frames.
+ * "move one cell every X"
+ */
 export const SNAKE_MOVE_DELAY = 10
+/**
+ * Size of the Canvas/Game view
+ */
 export const BOARD_SIZE = 400
+/**
+ * Impacts the number of cells in a row/column (R = C; RxC)
+ * Should be an odd number so that the Snake is positioned in the middle
+ */
 export const CELL_COUNT = 21
+/**
+ * Size of each cell
+ */
 export const CELL_SIZE  = BOARD_SIZE / CELL_COUNT
+/**
+ * Number of particles used/recycled withing the ParticlePool
+ */
 export const PARTICLE_POOL_SIZE = 20
